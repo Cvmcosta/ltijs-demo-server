2880940d35e62a146375dcac5ef0bcfb5a2c9ed3
carlos <cvmcosta@yahoo.com.br>
2020-07-22 23:34:25 -0300

    created v4 branch

---
+++ public/precache-manifest.0ce5b3c3c236c431c2ed289eefdac5df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee3611f30e535366d9522c12d79c750",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab7487d92d52270f7de0",
+    "url": "./static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "237d47e353ea056d426a",
+    "url": "./static/css/main.0e2988d0.chunk.css"
+  },
+  {
+    "revision": "ab7487d92d52270f7de0",
+    "url": "./static/js/2.9161100e.chunk.js"
+  },
+  {
+    "revision": "ab614938fe7d398190b8a985fec2e1f8",
+    "url": "./static/js/2.9161100e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237d47e353ea056d426a",
+    "url": "./static/js/main.bfc70ec6.chunk.js"
+  },
+  {
+    "revision": "b6d405b37e59947ce6c7",
+    "url": "./static/js/runtime-main.bb50f6a8.js"
+  }
+]);
