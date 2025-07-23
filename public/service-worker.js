bd6763b574a13632666f17ab5d709244c92bd429
Isabela Barotto <isabelacamillebarottojanguas@gmail.com>
2025-07-23 13:14:24 -0300

    código com implementação da LTI

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c983d65f44d85da6d9268a44c3e7a788.js"
+  "./precache-manifest.4c45889c3c33450f387b24f2f80a281c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
